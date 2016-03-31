9c405c89935b2b6244f5ab7a78ce9b6e33227940
denny483 <denny483@gmail.com>
2016-03-31 20:38:21 +0800

    Update SSLServer.java

--- SSLServer.java
+++ SSLServer.java
@@ -1,5 +1,7 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.URL;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateExpiredException;
@@ -10,41 +12,41 @@ import javax.net.ssl.SSLServerSocketFactory;
 import javax.net.ssl.SSLSocket;
 
 public class SSLServer {
-	public static void testConnectionTo(String aURL) throws Exception {
+  private static SSLSocket s;
+  public static void testConnectionTo(String aURL) throws Exception {
         URL destinationURL = new URL(aURL);
         HttpsURLConnection conn = (HttpsURLConnection) destinationURL
                 .openConnection();
         conn.connect();
         Certificate[] certs = conn.getServerCertificates();
+        OutputStreamWriter out = new OutputStreamWriter(s.getOutputStream());
+        BufferedWriter writer = new BufferedWriter(out);
         for (Certificate cert : certs) {
             System.out.println("Certificate is: " + cert);
             if(cert instanceof X509Certificate) {
                 try {
                     ( (X509Certificate) cert).checkValidity();
                     System.out.println("Certificate is active for current date");
+                    writer.write(cert.toString());
+                    writer.flush();
                 } catch(CertificateExpiredException cee) {
                     System.out.println("Certificate is expired");
+                    writer.write("Error");
+                    writer.flush();
                 }
             }
         }
     }
   public static void main(String[] arstring) {
-
     int port = 3000;
-
     try {
-      // 取得factory : 亦可由SSLContext產生
       SSLServerSocketFactory f = (SSLServerSocketFactory) SSLServerSocketFactory
           .getDefault();
       SSLServerSocket server = (SSLServerSocket) f.createServerSocket(port);
-      SSLSocket s = (SSLSocket) server.accept();
-
-      // 使用anonymous的cipher suite (測試用)
+      s = (SSLSocket) server.accept();
       s.setEnabledCipherSuites(s.getSupportedCipherSuites());
-
       InputStreamReader in = new InputStreamReader(s.getInputStream());
       BufferedReader reader = new BufferedReader(in);
-
       String str = null;
       while ((str = reader.readLine()) != null) {
         testConnectionTo(str);
